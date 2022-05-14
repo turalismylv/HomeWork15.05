2eff950be42d6d3ee48428d1e04fb32fa4f5cc00
turalismylv <turalhi@code.edu.az>
2022-05-14 21:51:06 +0400

    HomeWork1505

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //2. Parametrinde array qebul edib, arrayin ededlerinin cemin tapan ve o cemin 2e bolunub bolunmemesini geri qaytaran bir method yazin
+
+            //    int[] arr = { 1,2,3,4,5,6,7,8,9,10 };
+            //    int a = Calc(arr);
+            //    if (a%2==0) {
+            //        Console.WriteLine(a / 2);
+            //    }
+            //    else {
+            //        Console.WriteLine("Cem 2 e bolunmur");
+            //        }
+            //}
+            //static int Calc(int[] arr)
+            //{
+            //    int sum = 0;
+
+            //    for (int i = 0; i < arr.Length; i++)
+            //    {
+
+            //        sum += arr[i];
+            //    }
+
+            //    return sum;
+
+            //3.parametrinde en ve hundurluk alib, asagidaki kimi bir piramida cap eden method yazin.meselen en 5 , hundurluk 5->
+
+            //*
+            //**
+            //***
+            //****
+            //*****
+            //    Print(5, 5);
+            //}
+            //static void Print(int hundurluk, int en)
+            //{
+            //    for (int i = 0; i < hundurluk; i++)
+            //    {
+            //        for (int j = 0; j < en; j++)
+            //        {
+            //            if (j <= i)
+            //            {
+            //                Console.Write("* ");
+            //            }
+            //        }
+
+            //        Console.WriteLine("");
+            //    }
+           
+            
+        
+        }
+
+
+    }
+
+}
+    
+
